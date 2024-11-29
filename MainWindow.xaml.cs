fc34af6435d63be4b413d4761519b42e129fa8b5
VTischer <viktor.t1@schule.bremen.de>
2024-11-29 11:16:20 +0100

    Refactoring

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,48 +4,49 @@ using System.Windows.Media;
 
 namespace Game4Kids {
     public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-            AssignRandomCross();
-        }
-
         private Button buttonWithCross;
         private List<Button> buttonList = new List<Button>();
+        private readonly Random rand = new Random();
 
-        private void AssignRandomCross() {
+        public MainWindow() {
+            InitializeComponent();
+
             buttonList.Add(FirstButton);
             buttonList.Add(SecondButton);
             buttonList.Add(ThirdButton);
             buttonList.Add(FourthButton);
 
+            AssignRandomCross();
+        }
+
+        private void AssignRandomCross() {
             foreach (Button button in buttonList) {
                 button.Content = string.Empty;
             }
 
-            Random rand = new Random();
             int randomIndex = rand.Next(buttonList.Count);
             buttonWithCross = buttonList[randomIndex];
             buttonWithCross.Content = "X";
         }
 
         private Color GetRandomColor() {
-            Random rand = new Random();
             return Color.FromRgb((byte)rand.Next(256), (byte)rand.Next(256), (byte)rand.Next(256));
         }
 
         private void Grid_ButtonClick(object sender, RoutedEventArgs e) {
             if (e.OriginalSource is Button clickedButton) {
                 clickedButton.Background = new SolidColorBrush(GetRandomColor());
-                e.Handled = true;
 
                 PlayBeepSound();
+
                 if (clickedButton.Content.ToString() == "X") {
                     AssignRandomCross();
                 }
             }
         }
+
         private void PlayBeepSound() {
-            Console.Beep(1000, 500);
+            Task.Run(() => Console.Beep(1000, 500));
         }
     }
 }
